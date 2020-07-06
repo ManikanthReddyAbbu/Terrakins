454476a0156b83c9bdb193c03bcd10ca4f99f12a
Manikanth Reddy Abbu <53967637+ManikanthReddyAbbu@users.noreply.github.com>
2020-07-06 19:39:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 provider "aws" {
     region = "us-east-2"
+    shared_credentials_file = "~/.aws/credentials"
   /*access_key = var.aws_access_key
     secret_key = var.aws_secret_key
     shared_credentials_file = "/Users/tf_user/.aws/creds-2"
