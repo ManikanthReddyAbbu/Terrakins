86a96f19194bc1a6053a752cba2b86c87d32d46d
Manikanth Reddy Abbu <53967637+ManikanthReddyAbbu@users.noreply.github.com>
2022-04-26 02:07:44 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "myEC2_instance" {
         connection {
           type = "ssh"
           user = "ubuntu"
-          private_key = file("/usr/local/bin/Terrakins_master/terraform-jenkins.pem")
+          public_key = file("/usr/local/bin/Terrakins_master/terraform-jenkins.pem")
           host = "${aws_instance.myEC2_instance.public_ip}"
       }
     
