efd887df78005d13150fb6412e82b3aae2cb0b5e
Manikanth Reddy <reddymanikanth8@gmail.com>
2022-05-07 14:35:26 -0400

    OS_Demo

--- ec2.tf
+++ ec2.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "myEC2_instance" {
     resource "aws_vpc" "myvpc" {
     cidr_block = "10.10.0.0/16"
     tags = {
-        Name = "OS-Demo-VPC"
+        Name = "OS_Demo-VPC"
     }
 }    
  /*   
